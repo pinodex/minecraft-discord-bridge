ed006f439ab7600aa4173213b8423602873d15f3
Raphael Marco <hello@raphaelmarco.com>
2023-07-10 01:15:37 +0800

    Update avatar API

--- src/bridge.js
+++ src/bridge.js
@@ -1,11 +1,11 @@
-const events = require('./events');
+const events = require("./events");
 
-const { MinecraftLogListener } = require('./minecraft/log');
-const { getPlayerUuid, getPlayerAvatarUrl } = require('./minecraft/player');
-const RconWrapper = require('./minecraft/rcon-wrapper');
+const { MinecraftLogListener } = require("./minecraft/log");
+const { getPlayerAvatarUrl } = require("./minecraft/player");
+const RconWrapper = require("./minecraft/rcon-wrapper");
 
-const DiscordWebhookChatSender = require('./discord/chat-webhook-sender');
-const DiscordChatReceiver = require('./discord/chat-receiver');
+const DiscordWebhookChatSender = require("./discord/chat-webhook-sender");
+const DiscordChatReceiver = require("./discord/chat-receiver");
 
 const {
   MINECRAFT_LOG_FILE,
@@ -26,7 +26,7 @@ module.exports = () => {
   const rcon = new RconWrapper(MINECRAFT_RCON_HOST, MINECRAFT_RCON_PORT, MINECRAFT_RCON_PASSWORD);
 
   const discordNotificationSender = new DiscordWebhookChatSender(
-    DISCORD_MINECRAFT_NOTIFICATIONS_WEBHOOK_URL,
+    DISCORD_MINECRAFT_NOTIFICATIONS_WEBHOOK_URL
   );
 
   const discordChatSender = new DiscordWebhookChatSender(DISCORD_MINECRAFT_CHAT_WEBHOOK_URL);
@@ -34,14 +34,14 @@ module.exports = () => {
 
   const discordChatReceiver = new DiscordChatReceiver(
     DISCORD_MINECRAFT_BRIDGE_TOKEN,
-    DISCORD_MINECRAFT_CHAT_CHANNEL_ID,
+    DISCORD_MINECRAFT_CHAT_CHANNEL_ID
   );
 
   minecraft.on(events.MC_SERVER_STARTING, () => {
     discordStatusSender.sendGenericEmbedMessage([
       {
-        title: 'Starting Server',
-        description: 'The server is now starting. Please wait for a few seconds',
+        title: "Starting Server",
+        description: "The server is now starting. Please wait for a few seconds",
         color: 15105570,
       },
     ]);
@@ -50,8 +50,8 @@ module.exports = () => {
   minecraft.on(events.MC_SERVER_OPEN, () => {
     discordStatusSender.sendGenericEmbedMessage([
       {
-        title: 'Server is Up',
-        description: 'You can now connect to Minecraft Server',
+        title: "Server is Up",
+        description: "You can now connect to Minecraft Server",
         color: 5763719,
         footer: {
           text: `IP: ${MINECRAFT_HOST}`,
@@ -63,8 +63,8 @@ module.exports = () => {
   minecraft.on(events.MC_SERVER_CLOSED, () => {
     discordStatusSender.sendGenericEmbedMessage([
       {
-        title: 'Server is Down',
-        description: 'Server is temporarily down. see you later',
+        title: "Server is Down",
+        description: "Server is temporarily down. see you later",
         color: 15548997,
       },
     ]);
@@ -73,8 +73,8 @@ module.exports = () => {
   minecraft.on(events.MC_SERVER_CRASHED, ({ uid }) => {
     discordStatusSender.sendGenericEmbedMessage([
       {
-        title: 'Server Crashed',
-        description: 'The server will start soon',
+        title: "Server Crashed",
+        description: "The server will start soon",
         color: 15548997,
         footer: {
           text: `UID: ${uid}`,
@@ -84,68 +84,71 @@ module.exports = () => {
   });
 
   minecraft.on(events.MC_PLAYER_CHAT, async ({ username, message }) => {
-    const playerUuid = await getPlayerUuid(username);
-    const avatar = getPlayerAvatarUrl(playerUuid);
+    const avatar = getPlayerAvatarUrl(username);
 
     await discordChatSender.sendPlayerMessage(username, avatar, message);
   });
 
   minecraft.on(events.MC_PLAYER_JOINED, async ({ username }) => {
-    const playerUuid = await getPlayerUuid(username);
-    const avatar = getPlayerAvatarUrl(playerUuid, 24);
+    const avatar = getPlayerAvatarUrl(username, 24);
 
-    await discordNotificationSender.sendGenericEmbedMessage([{
-      description: `**${username}** joined the game`,
-      color: 5763719,
-      thumbnail: {
-        url: avatar,
+    await discordNotificationSender.sendGenericEmbedMessage([
+      {
+        description: `**${username}** joined the game`,
+        color: 5763719,
+        thumbnail: {
+          url: avatar,
+        },
       },
-    }]);
+    ]);
   });
 
   minecraft.on(events.MC_PLAYER_ADVANCEMENT, async ({ username, advancement, type }) => {
     const types = {
       goal: {
-        text: 'has reached the goal',
+        text: "has reached the goal",
         color: 5763719,
       },
       advancement: {
-        text: 'has made an advancement',
+        text: "has made an advancement",
         color: 5763719,
       },
       challenge: {
-        text: 'has completed the challenge',
+        text: "has completed the challenge",
         color: 7419530,
       },
     };
 
     const { text, color } = types[type];
 
-    await discordNotificationSender.sendGenericEmbedMessage([{
-      title: advancement,
-      description: `**${username}** ${text}.`,
-      color,
-    }]);
+    await discordNotificationSender.sendGenericEmbedMessage([
+      {
+        title: advancement,
+        description: `**${username}** ${text}.`,
+        color,
+      },
+    ]);
   });
 
   minecraft.on(events.MC_PLAYER_LEFT, async ({ username }) => {
-    const playerUuid = await getPlayerUuid(username);
-    const avatar = getPlayerAvatarUrl(playerUuid, 24);
+    const avatar = getPlayerAvatarUrl(username, 24);
 
-    await discordNotificationSender.sendGenericEmbedMessage([{
-      description: `**${username}** left the game`,
-      color: 15548997,
-      thumbnail: {
-        url: avatar,
+    await discordNotificationSender.sendGenericEmbedMessage([
+      {
+        description: `**${username}** left the game`,
+        color: 15548997,
+        thumbnail: {
+          url: avatar,
+        },
       },
-    }]);
+    ]);
   });
 
   discordChatReceiver.on(events.DISCORD_USER_CHAT, async ({ username, message }) => {
     await rcon.sendMessage(username, message);
   });
 
-  discordChatReceiver.addCommandHandler('playerlist', () => rcon.sendCommand('list'));
+  discordChatReceiver.addCommandHandler("playerlist", () => rcon.sendCommand("list"));
 
   rcon.connect();
   discordChatReceiver.login();
--- src/minecraft/player.js
+++ src/minecraft/player.js
@@ -1,9 +1,9 @@
-const cacheManager = require('cache-manager');
-const axios = require('axios');
-const logger = require('../logger');
+const cacheManager = require("cache-manager");
+const axios = require("axios");
+const logger = require("../logger");
 
 const cache = cacheManager.caching({
-  store: 'memory',
+  store: "memory",
   max: 100,
   ttl: 3600,
 });
@@ -12,7 +12,7 @@ const cache = cacheManager.caching({
  * Generic Minecraft Player ID
  * @type {String}
  */
-const STEVE_PLAYER_ID = '8667ba71b85a4004af54457a9734eed7';
+const STEVE_PLAYER_ID = "8667ba71b85a4004af54457a9734eed7";
 
 /**
  * Get Minecraft Player UUID
@@ -42,16 +42,16 @@ async function getPlayerUuid(username) {
 /**
  * Get Minecraft Player Avatar URL
  *
- * @param  {String} uuid Minecraft Player UUID
+ * @param  {String} user Minecraft Player username or UUID
  * @param  {String} size Minecraft Player Avatar Size
  * @return {String}
  */
-function getPlayerAvatarUrl(uuid, size = 100) {
-  if (uuid) {
-    return `https://crafatar.com/avatars/${uuid}?size=${size}`;
+function getPlayerAvatarUrl(user, size = 100) {
+  if (user) {
+    return `https://minotar.net/avatar/${user}?size=${size}`;
   }
 
-  return `https://crafatar.com/avatars/${STEVE_PLAYER_ID}?size=${size}`;
+  return `https://minotar.net/avatar/${STEVE_PLAYER_ID}?size=${size}`;
 }
 
 module.exports = {
