017b8f3ff8de97224e77f934fde43c4a393e121d
peraltadavidr <peraltadavidr@outlook.com>
2025-02-09 00:58:12 +0800

    update log patterns

--- src/bridge.js
+++ src/bridge.js
@@ -1,11 +1,11 @@
-const events = require("./events");
+const events = require('./events');
 
-const { MinecraftLogListener } = require("./minecraft/log");
-const { getPlayerAvatarUrl } = require("./minecraft/player");
-const RconWrapper = require("./minecraft/rcon-wrapper");
+const { MinecraftLogListenerSFTP } = require('./minecraft/log');
+const { getPlayerAvatarUrl } = require('./minecraft/player');
+const RconWrapper = require('./minecraft/rcon-wrapper');
 
-const DiscordWebhookChatSender = require("./discord/chat-webhook-sender");
-const DiscordChatReceiver = require("./discord/chat-receiver");
+const DiscordWebhookChatSender = require('./discord/chat-webhook-sender');
+const DiscordChatReceiver = require('./discord/chat-receiver');
 
 const {
   MINECRAFT_LOG_FILE,
@@ -18,15 +18,26 @@ const {
   DISCORD_MINECRAFT_NOTIFICATIONS_WEBHOOK_URL,
   DISCORD_MINECRAFT_BRIDGE_TOKEN,
   DISCORD_MINECRAFT_STATUS_WEBHOOK_URL,
+  MINECRAFT_SFTP_HOST,
+  MINECRAFT_SFTP_PORT,
+  MINECRAFT_SFTP_USERNAME,
+  MINECRAFT_SFTP_PASSWORD,
 } = process.env;
 
+const sftpConfig = {
+  host: MINECRAFT_SFTP_HOST,
+  port: MINECRAFT_SFTP_PORT,
+  username: MINECRAFT_SFTP_USERNAME,
+  password: MINECRAFT_SFTP_PASSWORD,
+};
+
 module.exports = () => {
-  const minecraft = new MinecraftLogListener(MINECRAFT_LOG_FILE);
+  const minecraft = new MinecraftLogListenerSFTP(sftpConfig, MINECRAFT_LOG_FILE);
 
   const rcon = new RconWrapper(MINECRAFT_RCON_HOST, MINECRAFT_RCON_PORT, MINECRAFT_RCON_PASSWORD);
 
   const discordNotificationSender = new DiscordWebhookChatSender(
-    DISCORD_MINECRAFT_NOTIFICATIONS_WEBHOOK_URL
+    DISCORD_MINECRAFT_NOTIFICATIONS_WEBHOOK_URL,
   );
 
   const discordChatSender = new DiscordWebhookChatSender(DISCORD_MINECRAFT_CHAT_WEBHOOK_URL);
@@ -34,14 +45,14 @@ module.exports = () => {
 
   const discordChatReceiver = new DiscordChatReceiver(
     DISCORD_MINECRAFT_BRIDGE_TOKEN,
-    DISCORD_MINECRAFT_CHAT_CHANNEL_ID
+    DISCORD_MINECRAFT_CHAT_CHANNEL_ID,
   );
 
   minecraft.on(events.MC_SERVER_STARTING, () => {
     discordStatusSender.sendGenericEmbedMessage([
       {
-        title: "Starting Server",
-        description: "The server is now starting. Please wait for a few seconds",
+        title: 'Starting Server',
+        description: 'The server is now starting. Please wait for a few seconds',
         color: 15105570,
       },
     ]);
@@ -50,8 +61,8 @@ module.exports = () => {
   minecraft.on(events.MC_SERVER_OPEN, () => {
     discordStatusSender.sendGenericEmbedMessage([
       {
-        title: "Server is Up",
-        description: "You can now connect to Minecraft Server",
+        title: 'Server is Up',
+        description: 'You can now connect to Minecraft Server',
         color: 5763719,
         footer: {
           text: `IP: ${MINECRAFT_HOST}`,
@@ -63,8 +74,8 @@ module.exports = () => {
   minecraft.on(events.MC_SERVER_CLOSED, () => {
     discordStatusSender.sendGenericEmbedMessage([
       {
-        title: "Server is Down",
-        description: "Server is temporarily down. see you later",
+        title: 'Server is Down',
+        description: 'Server is temporarily down. see you later',
         color: 15548997,
       },
     ]);
@@ -73,8 +84,8 @@ module.exports = () => {
   minecraft.on(events.MC_SERVER_CRASHED, ({ uid }) => {
     discordStatusSender.sendGenericEmbedMessage([
       {
-        title: "Server Crashed",
-        description: "The server will start soon",
+        title: 'Server Crashed',
+        description: 'The server will start soon',
         color: 15548997,
         footer: {
           text: `UID: ${uid}`,
@@ -106,15 +117,15 @@ module.exports = () => {
   minecraft.on(events.MC_PLAYER_ADVANCEMENT, async ({ username, advancement, type }) => {
     const types = {
       goal: {
-        text: "has reached the goal",
+        text: 'has reached the goal',
         color: 5763719,
       },
       advancement: {
-        text: "has made an advancement",
+        text: 'has made an advancement',
         color: 5763719,
       },
       challenge: {
-        text: "has completed the challenge",
+        text: 'has completed the challenge',
         color: 7419530,
       },
     };
@@ -148,7 +159,7 @@ module.exports = () => {
     await rcon.sendMessage(username, message);
   });
 
-  discordChatReceiver.addCommandHandler("playerlist", () => rcon.sendCommand("list"));
+  discordChatReceiver.addCommandHandler('playerlist', () => rcon.sendCommand('list'));
 
   rcon.connect();
   discordChatReceiver.login();
--- src/minecraft/log.js
+++ src/minecraft/log.js
@@ -1,4 +1,6 @@
+// eslint-disable-next-line max-classes-per-file
 const EventEmitter = require('events');
+const Client = require('ssh2-sftp-client');
 const { Tail } = require('tail');
 const logger = require('../logger');
 const events = require('../events');
@@ -126,7 +128,7 @@ const matchRules = [
     type: events.MC_PLAYER_JOINED,
     /* eslint no-useless-escape: off */
     pattern:
-      /\[(.*)\] \[Server thread\/INFO\] \[minecraft\/MinecraftServer\]: (\w+) joined the game/,
+      /\[(.*)\] \[Server thread\/INFO\] \[net\.minecraft\.server\.MinecraftServer\/\]: (\w+) joined the game/,
     handler(matches) {
       const [timestamp, username] = matches.slice(1);
 
@@ -137,7 +139,7 @@ const matchRules = [
     type: events.MC_PLAYER_LEFT,
     /* eslint no-useless-escape: off */
     pattern:
-      /\[(.*)\] \[Server thread\/INFO\] \[minecraft\/MinecraftServer\]: (\w+) left the game/,
+      /\[(.*)\] \[Server thread\/INFO\] \[net\.minecraft\.server\.MinecraftServer\/\]: (\w+) left the game/,
     handler(matches) {
       const [timestamp, username] = matches.slice(1);
 
@@ -204,4 +206,133 @@ class MinecraftLogListener extends EventEmitter {
   }
 }
 
-module.exports = { MinecraftLogListener, events };
+class MinecraftLogListenerSFTP extends EventEmitter {
+  /**
+   * Constructs MinecraftLogListener
+   // eslint-disable-next-line max-len
+   * @param  {Object} sftpConfig Configuration for SFTP connection (host, port, username, password or privateKey, etc.)
+   * @param  {String} remoteLogFile Full path to the remote Minecraft log file
+   */
+  constructor(sftpConfig, remoteLogFile) {
+    super();
+
+    this.sftpConfig = sftpConfig;
+    this.remoteLogFile = remoteLogFile;
+    this.pollInterval = 500; // Poll every 500ms (adjust as needed)
+    this.offset = 0; // Current read position in the file
+    this.buffer = ''; // Buffer to hold any incomplete log line
+
+    this.sftp = new Client();
+    this.connectSftp();
+  }
+
+  async connectSftp() {
+    try {
+      await this.sftp.connect(this.sftpConfig);
+      logger.info(`Connected to SFTP server: ${this.sftpConfig.host}`);
+
+      // Start from the current end of the file (ignore any pre-existing content)
+      const stat = await this.sftp.stat(this.remoteLogFile);
+      this.offset = stat.size;
+      logger.info(`Starting at offset ${this.offset} for ${this.remoteLogFile}`);
+
+      // Begin polling for new log data
+      this.poller = setInterval(() => {
+        this.pollLog().catch((err) => {
+          logger.error('Error polling remote log:', err);
+        });
+      }, this.pollInterval);
+    } catch (err) {
+      logger.error('SFTP connection error:', err);
+      this.emit('error', err);
+    }
+  }
+
+  async pollLog() {
+    try {
+      // Get the latest file stats
+      const stat = await this.sftp.stat(this.remoteLogFile);
+      if (stat.size > this.offset) {
+        // Calculate the new data length and update the offset
+        const start = this.offset;
+        const end = stat.size - 1; // end is inclusive
+        this.offset = stat.size;
+
+        // Create a read stream for the new chunk of data
+        const stream = await this.sftp.createReadStream(this.remoteLogFile, { start, end });
+        let data = '';
+
+        stream.setEncoding('utf8');
+        stream.on('data', (chunk) => {
+          data += chunk;
+        });
+        stream.on('error', (err) => {
+          logger.error('Error reading remote log file:', err);
+        });
+        stream.on('end', () => {
+          this.processData(data);
+        });
+      }
+    } catch (err) {
+      logger.error('Error during pollLog:', err);
+    }
+  }
+
+  /**
+   * Process new data from the remote log file.
+   * Splits data into lines, handling any incomplete line from the previous poll.
+   *
+   * @param {String} data New data read from the remote log file
+   */
+  processData(data) {
+    if (!data) return;
+
+    // Prepend any leftover incomplete line from last time.
+    data = this.buffer + data;
+    const lines = data.split(/\r?\n/);
+    // The last element might be an incomplete line.
+    this.buffer = lines.pop();
+
+    lines.forEach((line) => this.handleLine(line));
+  }
+
+  /**
+   * Handles a single log line.
+   *
+   * @param {String} line Log line
+   */
+  handleLine(line) {
+    if (typeof line !== 'string') {
+      return;
+    }
+
+    logger.debug(`Log line: ${line}`);
+
+    let matches = null;
+    const rule = matchRules.find(({ pattern }) => {
+      matches = line.match(pattern);
+      return matches;
+    });
+
+    if (!rule || !matches) {
+      return;
+    }
+
+    const data = rule.handler(matches);
+    if (data) {
+      logger.debug(`Minecraft Event [${rule.type}]: ${JSON.stringify(data)}`);
+      this.emit(rule.type, data);
+    }
+  }
+
+  /**
+   * Closes the SFTP connection and stops polling.
+   */
+  async close() {
+    clearInterval(this.poller);
+    await this.sftp.end();
+    logger.info('SFTP connection closed.');
+  }
+}
+
+module.exports = { MinecraftLogListener, MinecraftLogListenerSFTP, events };
