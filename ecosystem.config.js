40b3b39858e99963af4bae0b4d98dcecb59f2fa6
Raphael Marco <hello@raphaelmarco.com>
2025-03-11 12:10:00 +0800

    Remove housekeeping cron

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,11 +4,5 @@ module.exports = {
       name: 'minecraft-discord-bridge-bot',
       script: './index.js',
     },
-    {
-      name: 'minecraft-discord-bridge-housekeeping',
-      script: './index.js',
-      args: '--discord-housekeeping',
-      cron_restart: '0 8 * * *',
-    },
   ],
 };
